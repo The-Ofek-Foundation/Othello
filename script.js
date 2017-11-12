1560b07b1aba021e66e58c0bb19a8387b898979b
Ofek Gila <ofek@theofekfoundation.org>
2017-11-11 19:17:33 -0800

    Add hover

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ var dimensions = [8, 8];
 var boardui = getElemId("board");
 var brush = boardui.getContext("2d");
 
-var board;
+var board, turnGlobal;
 
 function pageReady() {
 	resizeBoard();
@@ -55,6 +55,8 @@ function newGame() {
 	board[parseInt(dimensions[0] / 2 - 0.5)][parseInt(dimensions[1] / 2 + 0.5)] = 1;
 	board[parseInt(dimensions[0] / 2 + 0.5)][parseInt(dimensions[1] / 2 - 0.5)] = 1;
 
+	turnGlobal = 0;
+
 	drawBoard();
 }
 
@@ -88,17 +90,20 @@ function drawGrid() {
 	brush.closePath();
 }
 
-function drawPiece(x, y) {
+function drawPiece(x, y, hover) {
 	if (board[x][y] === -1)
 		return;
+	opacity = hover ? 0.5:1;
+	let black = `rgba(0, 0, 0, ${opacity})`;
+	let white = `rgba(255, 255, 255, ${opacity})`;
 	switch (board[x][y]) {
 		case 0: // black
-			brush.fillStyle = 'black';
-			brush.strokeStyle = 'black';
+			brush.fillStyle = black;
+			brush.strokeStyle = 'transparent';
 			break;
 		case 1:
-			brush.fillStyle = 'white';
-			brush.strokeStyle = 'black';
+			brush.fillStyle = white;
+			brush.strokeStyle = black;
 			break;
 	}
 	brush.lineWidth = 4;
@@ -110,14 +115,65 @@ function drawPiece(x, y) {
 	brush.closePath();
 }
 
-function drawBoard() {
+function drawBoard(hoverMove) {
 	clearBoard();
 
 	for (var i = 0; i < board.length; i++)
 		for (var a = 0; a < board[i].length; a++)
-			drawPiece(i, a);
+			drawPiece(i, a, false);
+
+	if (hoverMove !== undefined) {
+		board[hoverMove[0]][hoverMove[1]] = turnGlobal;
+		drawPiece(hoverMove[0], hoverMove[1], true);
+		board[hoverMove[0]][hoverMove[1]] = -1;
+	}
 
 	drawGrid();
 }
 
+function getMove(xloc, yloc) {
+	var left = boardui.offsetLeft;
+	var top = boardui.offsetTop + contentWrapper.offsetTop;
+	if (xloc < left || xloc > left + boardWidth || yloc < top || yloc > boardHeight + top)
+		return [-1, -1];
+	return [Math.floor((xloc - left) / squareSize), Math.floor((yloc - top) / squareSize)];
+}
+
+function legalMove(tboard, x, y) {
+	if (tboard[x][y] !== -1)
+		return -1;
+	return 0;
+}
+
+function playMove(tboard, x, y, turn, startWith) {
+	tboard[x][y] = turn;
+}
+
+function setTurn(newTurn) {
+	turnGlobal = newTurn;
+	drawBoard();
+}
+
+boardui.addEventListener('mousedown', function (e) {
+	if (e.which === 3)
+		return;
+	let move = getMove(e.pageX, e.pageY);
+	let legality = legalMove(board, move[0], move[1]);
+	if (legality === -1)
+		return;
+
+	playMove(board, move[0], move[1], turnGlobal, legality);
+
+	setTurn((turnGlobal + 1) % 2);
+});
+
+boardui.addEventListener('mousemove', function (e) {
+	if (e.which === 3)
+		return;
+	let move = getMove(e.pageX, e.pageY);
+	if (legalMove(board, move[0], move[1]) === -1)
+		return;
+
+	drawBoard(move);
+});
 
